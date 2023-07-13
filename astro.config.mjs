40eaaf28a504ab01e0ef557b05304d9cc214de8a
Ruben <rubenca2312@gmail.com>
2023-07-13 11:45:15 +0200

    start shop development

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Elcheno.github.io',
-    base: '/clothingShop_Astro',
+    // site: 'https://Elcheno.github.io',
+    // base: '/clothingShop_Astro',
 });
